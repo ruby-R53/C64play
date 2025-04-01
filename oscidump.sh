fe0eaec7da634dce77694b2b96a1b66b66b607ff
ruby-R53 <enkiferreiracosta867@gmail.com>
2025-04-01 19:16:54 -0300

    fix oscidump to work with relative paths

--- oscidump.sh
+++ oscidump.sh
@@ -4,11 +4,9 @@
 DUMPS="/home/$USER/Videos/corr/dumps/C64/"
 
 if [[ $# != 0 ]]; then
-	cd $DUMPS # go to the proper directory first
-
-	c64play $@ -m2 -m3 -w01\ -\ Voice\ 1.wav && # then dump Voice 1
-	c64play $@ -m1 -m3 -w02\ -\ Voice\ 2.wav && # Voice 2
-	c64play $@ -m1 -m2 -w03\ -\ Voice\ 3.wav && # and 3
+	c64play $@ -m2 -m3 -w$DUMPS/01\ -\ Voice\ 1.wav && # then dump Voice 1
+	c64play $@ -m1 -m3 -w$DUMPS/02\ -\ Voice\ 2.wav && # Voice 2
+	c64play $@ -m1 -m2 -w$DUMPS/03\ -\ Voice\ 3.wav && # and 3
 
 	# exclude '-ma' from the arguments so that
 	# the master audio and sample channel get
@@ -19,11 +17,11 @@ if [[ $# != 0 ]]; then
 				[[ ! $PARAMS = "-ma" ]] && NEW_ARGS+=("$PARAMS")
 			done
 			set -- "${NEW_ARGS[@]}"
-			c64play $@ -m1 -m2 -m3 -w04\ -\ Master\ Volume.wav
+			c64play $@ -m1 -m2 -m3 -w$DUMPS/04\ -\ Master\ Volume.wav
 		fi
 	done
 
-	c64play $@ -w00\ -\ Master.wav &&
+	c64play $@ -w$DUMPS/00\ -\ Master.wav &&
 
 	echo "Done."
 else
